7ecbaa50c6950663f7224e3a1fa0cb7470a22ad5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2019-03-08 15:15:32 +0000

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '413659292776366',
+        clientSecret: 'a124f0afbdf0f09782963a3665ed31e6'
+    }
 }
